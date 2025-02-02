a860f0959314faf5726caac1d8ed4dd6af023427
Máté Tamási <kresho17@gmail.com>
2025-02-02 21:07:33 +0100

    slider arrow style

--- frontend/src/pages/ProductDetail/slider.tsx
+++ frontend/src/pages/ProductDetail/slider.tsx
@@ -34,20 +34,20 @@ export function ProductSlider() {
           </div>
         )
     }
-
+    /* TODO: Use color for the arrow which is active */
     return(
         <>
-                <Slider {...settings} className="w-4/12">
-                    <div className="">
-                        <img src="https://s3-alpha-sig.figma.com/img/fbec/9e1f/d0ae465204d6e44c78606ea8844e40f9?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=iCBBh7wLrurphBSXj-CnWpYwT07s1CFEIEzylRsuhK98h9iNpPy0CHrwCp5TAL9gic0F-Yc5Juju3gEuC9UhgHYOE9QBMJoL5PfPoG0jMgcO~e6drMkGcspr3Jk0W987XRa5Z2qjYzhLtEPYU~CpbMis8DkZBcF4fQwXuDjcnD5gQ76ls21YkwGFNcq56scpzH5OtSwmpvkLZwyen6pApH8MXHxMsZW8nHu4GJJGhj1SmTa7IWMG94E66Th2~Z3~LwpfAjX~fHRXTU0e6a4IHsCCT2IkuqKsgBBxwlJGHj4py6c3NNxkmcDjvgTyE33I0JsmnpwaZSLw7rdMddqR1Q__" alt="Product Image 1" className="w-[502.4px] h-[481px] rounded-[6.01px] object-contain mx-auto"/>
-                    </div>
-                    <div className="">
-                        <img src="https://s3-alpha-sig.figma.com/img/fbec/9e1f/d0ae465204d6e44c78606ea8844e40f9?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=iCBBh7wLrurphBSXj-CnWpYwT07s1CFEIEzylRsuhK98h9iNpPy0CHrwCp5TAL9gic0F-Yc5Juju3gEuC9UhgHYOE9QBMJoL5PfPoG0jMgcO~e6drMkGcspr3Jk0W987XRa5Z2qjYzhLtEPYU~CpbMis8DkZBcF4fQwXuDjcnD5gQ76ls21YkwGFNcq56scpzH5OtSwmpvkLZwyen6pApH8MXHxMsZW8nHu4GJJGhj1SmTa7IWMG94E66Th2~Z3~LwpfAjX~fHRXTU0e6a4IHsCCT2IkuqKsgBBxwlJGHj4py6c3NNxkmcDjvgTyE33I0JsmnpwaZSLw7rdMddqR1Q__" alt="Product Image 2" className="w-[502.4px] h-[481px] rounded-[6.01px] object-contain mx-auto"/>
-                    </div>
-                    <div className="">
-                        <img src="https://s3-alpha-sig.figma.com/img/fbec/9e1f/d0ae465204d6e44c78606ea8844e40f9?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=iCBBh7wLrurphBSXj-CnWpYwT07s1CFEIEzylRsuhK98h9iNpPy0CHrwCp5TAL9gic0F-Yc5Juju3gEuC9UhgHYOE9QBMJoL5PfPoG0jMgcO~e6drMkGcspr3Jk0W987XRa5Z2qjYzhLtEPYU~CpbMis8DkZBcF4fQwXuDjcnD5gQ76ls21YkwGFNcq56scpzH5OtSwmpvkLZwyen6pApH8MXHxMsZW8nHu4GJJGhj1SmTa7IWMG94E66Th2~Z3~LwpfAjX~fHRXTU0e6a4IHsCCT2IkuqKsgBBxwlJGHj4py6c3NNxkmcDjvgTyE33I0JsmnpwaZSLw7rdMddqR1Q__" alt="Product Image 3" className="w-[502.4px] h-[481px] rounded-[6.01px] object-contain mx-auto"/>
-                    </div>
-                </Slider>
+          <Slider {...settings} className="w-4/12">
+              <div className=""> {/* TODO: object-contain */}
+                  <img src="https://s3-alpha-sig.figma.com/img/fbec/9e1f/d0ae465204d6e44c78606ea8844e40f9?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=iCBBh7wLrurphBSXj-CnWpYwT07s1CFEIEzylRsuhK98h9iNpPy0CHrwCp5TAL9gic0F-Yc5Juju3gEuC9UhgHYOE9QBMJoL5PfPoG0jMgcO~e6drMkGcspr3Jk0W987XRa5Z2qjYzhLtEPYU~CpbMis8DkZBcF4fQwXuDjcnD5gQ76ls21YkwGFNcq56scpzH5OtSwmpvkLZwyen6pApH8MXHxMsZW8nHu4GJJGhj1SmTa7IWMG94E66Th2~Z3~LwpfAjX~fHRXTU0e6a4IHsCCT2IkuqKsgBBxwlJGHj4py6c3NNxkmcDjvgTyE33I0JsmnpwaZSLw7rdMddqR1Q__" alt="Product Image 1" className="w-[502.4px] h-[481px] rounded-[6.01px] object-cover mx-auto"/>
+              </div>
+              <div className="">
+                  <img src="https://s3-alpha-sig.figma.com/img/fbec/9e1f/d0ae465204d6e44c78606ea8844e40f9?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=iCBBh7wLrurphBSXj-CnWpYwT07s1CFEIEzylRsuhK98h9iNpPy0CHrwCp5TAL9gic0F-Yc5Juju3gEuC9UhgHYOE9QBMJoL5PfPoG0jMgcO~e6drMkGcspr3Jk0W987XRa5Z2qjYzhLtEPYU~CpbMis8DkZBcF4fQwXuDjcnD5gQ76ls21YkwGFNcq56scpzH5OtSwmpvkLZwyen6pApH8MXHxMsZW8nHu4GJJGhj1SmTa7IWMG94E66Th2~Z3~LwpfAjX~fHRXTU0e6a4IHsCCT2IkuqKsgBBxwlJGHj4py6c3NNxkmcDjvgTyE33I0JsmnpwaZSLw7rdMddqR1Q__" alt="Product Image 2" className="w-[502.4px] h-[481px] rounded-[6.01px] object-cover mx-auto"/>
+              </div>
+              <div className="">
+                  <img src="https://s3-alpha-sig.figma.com/img/fbec/9e1f/d0ae465204d6e44c78606ea8844e40f9?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=iCBBh7wLrurphBSXj-CnWpYwT07s1CFEIEzylRsuhK98h9iNpPy0CHrwCp5TAL9gic0F-Yc5Juju3gEuC9UhgHYOE9QBMJoL5PfPoG0jMgcO~e6drMkGcspr3Jk0W987XRa5Z2qjYzhLtEPYU~CpbMis8DkZBcF4fQwXuDjcnD5gQ76ls21YkwGFNcq56scpzH5OtSwmpvkLZwyen6pApH8MXHxMsZW8nHu4GJJGhj1SmTa7IWMG94E66Th2~Z3~LwpfAjX~fHRXTU0e6a4IHsCCT2IkuqKsgBBxwlJGHj4py6c3NNxkmcDjvgTyE33I0JsmnpwaZSLw7rdMddqR1Q__" alt="Product Image 3" className="w-[502.4px] h-[481px] rounded-[6.01px] object-cover mx-auto"/>
+              </div>
+          </Slider>
         </>
     );
 }
