9386bce48b8639291afd80dbfaad938f071a6c71
Máté Tamási <kresho17@gmail.com>
2025-02-02 13:34:46 +0100

    Add ItemCard

---
+++ frontend/src/components/ItemCard.tsx
@@ -0,0 +1,43 @@
+import { truncateText } from "../utils/textUtils";
+import { ItemDeatils } from "../pages/Home/data";
+
+interface Props {
+    item: ItemDeatils
+}
+
+export function ItemCard({ item }: Props) {
+
+
+    return(
+        <div className="border-[0.65px] border-[#DBDBDB] w-[305px] h-[320px] rounded-[6.46px] bg-white   flex flex-col items-center justify-between p-[11.395px] pb-[25px]">
+            <div className="relative inline-block w-full h-[149.18px] rounded-[6.46px] overflow-hidden">
+                <img src={item.thumbnail} alt="item" className="object-contain w-full h-full rounded-[6.46px]"/> {/* TODO: In the figma design, the image should be object-cover */}
+                <button className="absolute top-[9.67px] right-[9.67px] text-[14px] text-white w-[83px] h-[30px] rounded-[13.88px] bg-[#6100FF] font-semibold">
+                    {item.discountPercentage}
+                </button>
+            </div>
+            {/* TEXT */}
+            <div className="w-full flex-grow flex flex-col items-center text-center justify-between pt-[11.395px] text-[#323232]">
+                {/* TITLE LINE */}
+                <div className="flex w-full justify-between items-center">
+                    <div className="text-[20px] font-[600] items-center">
+                        <p>{item.title}</p>
+                    </div>
+                    <div className="text-[24px] font-[600]">
+                        <p>{item.price}</p>
+                    </div>
+                </div>
+                {/* DESCRIPTION */}
+                <div className="w-full text-[14px] font-[500] height-[40px] text-left align-left pb-[10px]">
+                    <p className="w-10/12">{truncateText(item.description, 64)}</p>
+                </div>
+            </div>
+            {/* BUTTON */}
+            <div className="w-full">
+                <button className="w-[281.86px] h-[41px] rounded-[25.83px] bg-black text-white font-semibold text-[16px]">
+                    See details
+                </button>
+            </div>
+        </div>
+    );
+};
