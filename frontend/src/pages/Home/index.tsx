7621ef84585004c16dda6e5688ee903954f1b5ba
Máté Tamási <kresho17@gmail.com>
2025-02-02 20:30:03 +0100

    Home page style update

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -6,15 +6,16 @@ export function Home() {
 
     return (
         <>
-            <h1 className="text-[48px] font-semibold text-[#323232] pt-[50px] pb-[49px] text-center">See Products</h1>
-            <div className="flex justify-center items-center">
-                <div className="grid grid-cols-4 gap-x-[16px] gap-y-[21px]">
-                    {ItemData.map((item) => (
-                        <ItemCard item={item} />
-                    ))}
+            <div className="flex flex-col items-center">
+                <h1 className="text-[48px] font-semibold text-[#323232] pt-[50px] text-center">See Products</h1>
+                <div className="flex justify-center items-center py-[49px]">
+                    <div className="grid xl:grid-cols-4 gap-x-[16px] gap-y-[21px] lg:grid-cols-3 md:grid-cols-2 grid-col-1">
+                        {ItemData.map((item) => (
+                            <ItemCard item={item} />
+                        ))}
+                    </div>
                 </div>
-            </div>
-            
+            </div> 
         </>
     );
 }
