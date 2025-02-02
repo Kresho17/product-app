ebe0bf395949b1631a6a98a86bfe3043b14f9d8a
Máté Tamási <kresho17@gmail.com>
2025-02-02 13:35:17 +0100

    Add Grid product list

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,19 @@
+import { ItemCard } from "../../components/ItemCard";
+import { ItemData } from "./data";
 
 
 export function Home() {
 
     return (
         <>
+            <h1 className="text-[48px] font-semibold text-[#323232] pt-[50px] pb-[49px] text-center">See Products</h1>
+            <div className="flex justify-center items-center">
+                <div className="grid grid-cols-4 gap-x-[16px] gap-y-[21px]">
+                    {ItemData.map((item) => (
+                        <ItemCard item={item} />
+                    ))}
+                </div>
+            </div>
             
         </>
     );
