900e15016c43a01e9a6763141cdafcc8cb864f02
Máté Tamási <kresho17@gmail.com>
2025-02-03 18:19:10 +0100

    Add 404

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { ProductDetail } from './pages/ProductDetail';
+import { NotFound } from './pages/NotFound';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='product/:id' element={<ProductDetail />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </Router>
   )
