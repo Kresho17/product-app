732f5f395180b02c588234c46fb3ebf6da235cd9
Máté Tamási <kresho17@gmail.com>
2025-02-03 18:17:15 +0100

    API fetch

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,19 +1,66 @@
+import { useEffect, useState } from "react";
 import { ItemCard } from "../../components/ItemCard";
-import { ItemData } from "./data";
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+export interface ItemDeatils {
+    id:                 number;
+    title:              string;
+    description:        string;
+    discountPercentage: number;
+    price:              number;
+    rating:             number;
+    stock:              number;
+    brand:              string;
+    category:           string;
+    images:             string[];
+}
 
 
 export function Home() {
+    const [items, setItems] = useState<ItemDeatils[]>([]);
+    const [page, setPage] = useState<number>(0);
+    const [hasMore, setHasMore] = useState<boolean>(true);
+    const limit = 10;
+
+    useEffect(() => {
+        fetchItems(0);
+    }, []);
+
+    const fetchItems = async (pageNumber: number) => {
+        try {
+            const response = await fetch(`http://127.0.0.1:8000/api/products/?skip=${pageNumber * limit}&limit=${limit}`);
+            const data = await response.json();
+
+            if (data.results.length < limit) {
+                setHasMore(false);
+            }
+
+            setItems((prevItems) => [...prevItems, ...data.results]);
+            setPage((prevPage) => prevPage + 1);
+        } catch (error) {
+            console.error("Error fetching items:", error);
+        }
+    };
+
 
     return (
         <>
             <div className="flex flex-col items-center">
                 <h1 className="text-[48px] font-semibold text-[#323232] pt-[50px] text-center">See Products</h1>
                 <div className="flex justify-center items-center py-[49px]">
-                    <div className="grid xl:grid-cols-4 gap-x-[16px] gap-y-[21px] lg:grid-cols-3 md:grid-cols-2 grid-col-1">
-                        {ItemData.map((item) => (
-                            <ItemCard item={item} key={item.id}/>
-                        ))}
-                    </div>
+                    <InfiniteScroll
+                        dataLength={items.length}
+                        next={() => fetchItems(page)}
+                        hasMore={hasMore}
+                        loader={<h4 className="my-6" >Loading...</h4>}
+                        endMessage={<p className="my-6">No more products available</p>}
+                    >
+                        <div className="grid xl:grid-cols-4 gap-x-[16px] gap-y-[21px] lg:grid-cols-3 md:grid-cols-2 grid-col-1">
+                            {items.map((item) => (
+                                <ItemCard item={item} key={item.id}/>
+                            ))}
+                        </div>
+                    </InfiniteScroll>
                 </div>
             </div> 
         </>
