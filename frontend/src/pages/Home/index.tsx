0863b979930c2543ccc853984f3d148876a4d273
Máté Tamási <kresho17@gmail.com>
2025-02-03 10:10:25 +0100

    Add key to unique comp

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -11,7 +11,7 @@ export function Home() {
                 <div className="flex justify-center items-center py-[49px]">
                     <div className="grid xl:grid-cols-4 gap-x-[16px] gap-y-[21px] lg:grid-cols-3 md:grid-cols-2 grid-col-1">
                         {ItemData.map((item) => (
-                            <ItemCard item={item} />
+                            <ItemCard item={item} key={item.id}/>
                         ))}
                     </div>
                 </div>
